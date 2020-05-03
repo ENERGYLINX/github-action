588cca88995cf44015d9549f9d0a8cfd9e06f81c
Paul A.T. Wilson <paul.wilson@energylinx.co.uk>
2020-05-03 18:16:10 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -l
 
+echo "k1"
 echo "Key: ${!INPUT_AWS-ACCESS-KEY-ID}"
+echo "k2"
 echo "Key2: ${!INPUT_AWS-SECRET-ACCESS-KEY}"
-echo "Key3: $2"
+
+echo "aws config"
 aws configure set region eu-west-2 --profile dev-deploy
 aws configure set profile.dev-deploy.aws_access_key_id "${!INPUT_AWS-ACCESS-KEY-ID}"
 aws configure set profile.dev-deploy.aws_secret_access_key "${!INPUT_AWS-SECRET-ACCESS-KEY}"
