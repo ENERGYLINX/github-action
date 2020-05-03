391f035f155d4ba35406fcbeb2865888ae420383
Paul A.T. Wilson <paul.wilson@energylinx.co.uk>
2020-05-03 19:25:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,5 @@ echo "${AWS-SECRET-ACCESS-KEY}"
 aws configure set region eu-west-2 --profile dev-deploy
 aws configure set profile.dev-deploy.aws_access_key_id "${AWS-ACCESS-KEY-ID}"
 aws configure set profile.dev-deploy.aws_secret_access_key "${AWS-SECRET-ACCESS-KEY}"
-sudo npm i -g serverless
+npm i -g serverless
 serverless deploy --verbose --stage dev
